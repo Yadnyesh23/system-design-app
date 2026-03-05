d18dbacc9e75165277c3e6d74ad0d110ec54269f
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-03-05 22:55:13 +0530

    bug : CORS error

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,11 @@ import cors from 'cors'
 const app = express()
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+  origin: "https://system-design-app-tau.vercel.app/", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));app.use(express.json())
 
 import scenerioRoutes from '../src/routes/scenerio.route.js'
 app.use("/api/scenarios", scenerioRoutes)
