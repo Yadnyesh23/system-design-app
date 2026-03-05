5daaa7bf53e0a5b6d8ab46b42ca3614f50dddba6
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-03-06 00:03:03 +0530

    fix : app.js structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express"
+import cors from "cors"
+
 const app = express()
 
-// Middleware
 app.use(cors({
   origin: [
     "http://localhost:5173",
@@ -10,9 +10,12 @@ app.use(cors({
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
-}));app.use(express.json())
+}))
 
-import scenerioRoutes from '../src/routes/scenerio.route.js'
+app.use(express.json())
+
+import scenerioRoutes from "../src/routes/scenerio.route.js"
+
 app.use("/api/scenarios", scenerioRoutes)
 
 export default app
